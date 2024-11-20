394ff988b4ebe7d4e7b9d48273e039a47f5511f3
bjarne <bjarneblu@gmail.com>
2024-11-20 13:46:10 +0100

    reverted IDE troll

--- database/Domain/BaseClass.py
+++ database/Domain/BaseClass.py
@@ -1,4 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase
+# tell the IDE to ignore the import error, otherwise the Entity classes will not have any imported classes
+# noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
+from sqlalchemy import Column, BigInteger, VARCHAR, ForeignKey, DATETIME
+from sqlalchemy.orm import DeclarativeBase, relationship
+# noinspection PyUnresolvedReferences
+from sqlalchemy.sql import func
 
 
 class Base(DeclarativeBase):
