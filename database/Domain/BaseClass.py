97c8f7c8d10807f1fd09d96893e4a674e5f08885
bjarne <bjarneblu@gmail.com>
2024-11-20 13:47:13 +0100

    final reverted IDE troll

--- database/Domain/BaseClass.py
+++ database/Domain/BaseClass.py
@@ -1,4 +1,6 @@
-# tell the IDE to ignore the import error, otherwise the Entity classes will not have any imported classes
+"""
+tell the IDE to ignore the import error, otherwise the Entity classes will not have any imported classes
+"""
 # noinspection PyUnresolvedReferences
 from sqlalchemy import Column, BigInteger, VARCHAR, ForeignKey, DATETIME
 # noinspection PyUnresolvedReferences
