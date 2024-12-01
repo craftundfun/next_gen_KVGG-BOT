49ea74673a05743d5ec99b63bf8217a4449581ba
bjarne <bjarneblu@gmail.com>
2024-12-01 01:43:09 +0100

    better MemberManager

--- Bot/src/Member/MemberManager.py
+++ Bot/src/Member/MemberManager.py
@@ -1,16 +1,15 @@
 from datetime import datetime
 
-from discord import Member, User
-from sqlalchemy import select
+from discord import Member, User, RawMemberRemoveEvent
+from sqlalchemy import select, null
 from sqlalchemy.exc import NoResultFound
 
-from database.Domain.DiscordUser.Entity.DiscordUser import DiscordUser
-from database.Domain.Guild.Entity.Guild import Guild
-from database.Domain.Guild.Entity.GuildDiscordUserMapping import GuildDiscordUserMapping
-from src.Client.Client import Client
-from src.Database.DatabaseConnection import getSession
-from src.Logging.Logger import Logger
-from src.Types.ClientListenerType import ClientListenerType
+from Bot.database.Domain.DiscordUser.Entity.DiscordUser import DiscordUser
+from Bot.database.Domain.Guild.Entity.GuildDiscordUserMapping import GuildDiscordUserMapping
+from Bot.src.Client.Client import Client
+from Bot.src.Database.DatabaseConnection import getSession
+from Bot.src.Logging.Logger import Logger
+from Bot.src.Types.ClientListenerType import ClientListenerType
 
 logger = Logger("MemberManager")
 
@@ -32,8 +31,9 @@ class MemberManager:
         self.client.addListener(self.memberJoinGuild, ClientListenerType.MEMBER_JOIN)
         logger.debug("Registered member join listener")
 
-        self.client.addListener(self.memberLeftGuild, ClientListenerType.MEMBER_REMOVE)
-        logger.debug("Registered member remove listener")
+        """the raw event is called everytime, use that to avoid unnecessary database calls"""
+        # self.client.addListener(self.memberLeftGuild, ClientListenerType.MEMBER_REMOVE)
+        # logger.debug("Registered member remove listener")
 
         self.client.addListener(self.memberLeftGuildRaw, ClientListenerType.RAW_MEMBER_REMOVE)
         logger.debug("Registered raw member remove listener")
@@ -61,36 +61,51 @@ class MemberManager:
                 )
 
                 self.session.add(databaseMember)
+                logger.debug(f"Member {member.name, member.id} added to database")
             except Exception as error:
+                self.session.rollback()
                 logger.error(f"Failed to get member {member.name, member.id}", exc_info=error)
 
                 return
             else:
                 logger.debug(f"Member {member.name, member.id} found in database")
 
-            # Get the guild from the database
+            # Fetch old guild member mapping or create a new one
             try:
-                selectQuery = (select(Guild).where(Guild.guild_id == member.guild.id))
-                databaseGuild = self.session.execute(selectQuery).scalars().one()
+                selectQuery = (select(GuildDiscordUserMapping)
+                               .where(GuildDiscordUserMapping.guild_id == member.guild.id,
+                                      GuildDiscordUserMapping.discord_user_id == member.id, ))
+                guildDiscordUserMapping = self.session.execute(selectQuery).scalars().one()
+            except NoResultFound:
+                try:
+                    guildDiscordUserMapping = GuildDiscordUserMapping(
+                        guild_id=member.guild.id,
+                        discord_user_id=databaseMember.discord_id,
+                        display_name=member.display_name,
+                    )
+
+                    self.session.add(guildDiscordUserMapping)
+                    logger.debug(f"Member {member.name, member.id} added to guild {member.guild.name, member.guild.id}")
+                except Exception as error:
+                    logger.error(f"Failed to add member {member.name, member.id} to guild "
+                                 f"{member.guild.name, member.guild.id}",
+                                 exc_info=error, )
+                    self.session.rollback()
+
+                    return
             except Exception as error:
-                logger.error(f"Failed to get guild {member.guild.name, member.guild.id}", exc_info=error)
+                logger.error(f"Failed to get guild member mapping for {member.name, member.id} "
+                             f"and {member.guild.name, member.guild.id}",
+                             exc_info=error, )
 
                 return
             else:
-                logger.debug(f"Guild {member.guild.name, member.guild.id} found in database")
+                guildDiscordUserMapping.left_at = null()
 
-            # Add the member to the guild
             try:
-                guildDiscordUserMapping = GuildDiscordUserMapping(
-                    guild_id=databaseGuild.guild_id,
-                    discord_user_id=databaseMember.discord_id,
-                    display_name=member.display_name,
-                )
-
-                self.session.add(guildDiscordUserMapping)
                 self.session.commit()
             except Exception as error:
-                logger.error(f"Failed to add member {member.name, member.id} to guild "
+                logger.error(f"Failed to add or edit member {member.name, member.id} for guild "
                              f"{member.guild.name, member.guild.id}",
                              exc_info=error, )
                 self.session.rollback()
@@ -124,14 +139,16 @@ class MemberManager:
             else:
                 logger.debug(f"Member {member.name, member.id} removed from guild {member.guild.name, member.guild.id}")
 
-    async def memberLeftGuildRaw(self, guildId: int, user: User):
+    async def memberLeftGuildRaw(self, payload: RawMemberRemoveEvent):
         """
         Remove a member from the guild using the user object because the member object is not available
 
-        :param guildId: Guild ID
-        :param user: User object
+        :param payload: Raw member remove event
         :return:
         """
+        guildId: int = payload.guild_id
+        user: User = payload.user
+
         with self.session:
             try:
                 selectQuery = (select(GuildDiscordUserMapping)
