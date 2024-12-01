9f84788405227ac963df9e1173b6e582db10f8de
bjarne <bjarneblu@gmail.com>
2024-12-01 20:41:03 +0100

    working discord oauth in the backend

--- main_backend.py
+++ main_backend.py
@@ -2,12 +2,13 @@
 import os
 
 import pymysql
+import requests
 from flask_sqlalchemy import SQLAlchemy
 
 from database.Domain.BaseClass import Base
 
 pymysql.install_as_MySQLdb()
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -24,7 +25,37 @@ db.init_app(app)
 
 @app.route('/api/hello')
 def hello_world():
-    return jsonify(message="Hello from Flask! :)")
+    return jsonify(message="Hello from Bjarne! :)")
+
+
+@app.route('/auth/discord')
+def test():
+    code = request.args.get("code")
+
+    if code is None:
+        return jsonify(message="Code is missing"), 400
+
+    url = "https://discord.com/api/oauth2/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "client_id": os.getenv("CLIENT_ID"),
+        "client_secret": os.getenv("CLIENT_SECRET"),
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": "http://127.0.0.1:8000/auth/discord"
+    }
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+
+    headers = {
+        "Authorization": f"{response.json()['token_type']} {response.json()['access_token']}",
+    }
+    response = requests.get("https://discord.com/api/users/@me", headers=headers)
+    response.raise_for_status()
+
+    return jsonify(response.json())
 
 
 if __name__ == '__main__':
--- web/src/App.js
+++ web/src/App.js
@@ -6,12 +6,28 @@ function App() {
 	useEffect(() => {
 		fetch('http://localhost:8000/api/hello')
 			.then(response => response.json())
-			.then(data => setMessage(data.message + " And hello from React! :)"));
+			.then(data => setMessage(data.message + " And hello from Rene! :)"));
 	}, []);
+
+	// return (
+	// 	<div className="App">
+	// 		<h1>{message}</h1>
+	// 	</div>
+	// );
 
 	return (
 		<div className="App">
-			<h1>{message}</h1>
+			<h1>Show and tell</h1>
+			<button
+				onClick={(e) =>
+					window.open(
+						"https://discord.com/oauth2/authorize?client_id=1076510738576855051&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fauth%2Fdiscord&scope=identify",
+						"_parent"
+					)
+				}
+			>
+				Auth
+			</button>
 		</div>
 	);
 }
