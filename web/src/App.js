08edf03ea2b9ecadd2639932663c05bc3fdece59
bjarne <bjarneblu@gmail.com>
2024-12-01 04:45:15 +0100

    frontend

---
+++ web/src/App.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+
+function App() {
+	const [message, setMessage] = useState("");
+
+	useEffect(() => {
+		fetch('http://localhost:5000/api/hello')
+			.then(response => response.json())
+			.then(data => setMessage(data.message));
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>{message}</h1>
+		</div>
+	);
+}
+
+export default App;
