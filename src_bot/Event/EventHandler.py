b252de27781867b0d39388bffa50adf3bc6e9c63
Bjarne <bjarneblu@gmail.com>
2024-12-08 19:48:39 +0100

    more compact and race condition secure EventHandler

--- src_bot/Event/EventHandler.py
+++ src_bot/Event/EventHandler.py
@@ -1,10 +1,13 @@
 from discord import Member, VoiceState
 
+from asyncio.locks import Lock
 from database.Domain.models.History import History
 from src_bot.Client.Client import Client
 from src_bot.Database.DatabaseConnection import getSession
 from src_bot.Logging.Logger import Logger
 from src_bot.Types.ClientListenerType import ClientListenerType
+
+from sqlalchemy import null
 
 logger = Logger("EventHandler")
 
@@ -14,6 +17,7 @@ class EventHandler:
     def __init__(self, client: Client):
         self.client = client
         self.session = getSession()
+        self.lock = Lock()
 
         self.registerListeners()
 
@@ -33,217 +37,109 @@ class EventHandler:
         :param before: The voice state before the update.
         :param after: The voice state after the update.
         """
-        if member.bot:
-            logger.debug(f"{member.display_name} is a bot, ignoring")
+        async with self.lock:
+            def getHistoryObject(eventId: int, additionalInfo: dict | None):
+                """
+                Create a history object for the given event and additional info.
 
-            return
+                :param eventId: The event id of the history.
+                :param additionalInfo: Additional info for the history.
+                """
+                return History(
+                    discord_id=member.id,
+                    guild_id=member.guild.id,
+                    event_id=eventId,
+                    additional_info=additionalInfo if additionalInfo else null(),
+                )
 
-        histories = []
+            if member.bot:
+                logger.debug(f"{member.display_name} is a bot, ignoring")
 
-        # Member joined a voice channel
-        if not before.channel and after.channel:
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=7,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
+                return
+
+            histories = []
 
             # insert first to complete the join-leave-circle
-            histories.append(history)
+            # Member joined a voice channel
+            if not before.channel and after.channel:
+                histories.append(getHistoryObject(7, {"channel_id": after.channel.id}))
 
-        # Member moved to another voice channel
-        if before.channel and after.channel and before.channel.id != after.channel.id:
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=9,
-                additional_info={
-                    "channel_from": before.channel.id,
-                    "channel_to": after.channel.id,
-                },
-            )
+            # Member moved to another voice channel
+            if before.channel and after.channel and before.channel.id != after.channel.id:
+                histories.append(
+                    getHistoryObject(9, {"channel_from": before.channel.id, "channel_to": after.channel.id}))
 
-            histories.append(history)
+            # Member muted themselves or was muted
+            if (after.self_mute or after.mute) and not (before.self_mute or before.mute):
+                histories.append(getHistoryObject(1, {"channel_id": after.channel.id}))
 
-        # Member left a voice channel
-        if before.channel and not after.channel:
-            historyLeave = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=8,
-                additional_info={
-                    "channel_id": before.channel.id,
-                },
-            )
+            # Member joined a voice channel muted
+            if not before.channel and after.channel and (after.self_mute or after.mute):
+                histories.append(getHistoryObject(1, {"channel_id": after.channel.id}))
 
-            # unmute member upon leaving
-            if before.self_mute or before.mute:
-                history = History(
-                    discord_id=member.id,
-                    guild_id=member.guild.id,
-                    event_id=2,
-                    additional_info={
-                        "channel_id": before.channel.id,
-                    },
-                )
+            # Member unmuted themselves or was unmuted
+            if not (after.self_mute or after.mute) and (before.self_mute or before.mute):
+                histories.append(getHistoryObject(2, {"channel_id": after.channel.id}))
 
-                histories.append(history)
+            # Member deafened themselves or was deafened
+            if (after.self_deaf or after.deaf) and not (before.self_deaf or before.deaf):
+                histories.append(getHistoryObject(3, {"channel_id": after.channel.id}))
 
-            # undeafen member upon leaving
-            if before.self_deaf or before.deaf:
-                history = History(
-                    discord_id=member.id,
-                    guild_id=member.guild.id,
-                    event_id=4,
-                    additional_info={
-                        "channel_id": before.channel.id,
-                    },
-                )
+            # Member joined a voice channel deafened
+            if not before.channel and after.channel and (after.self_deaf or after.deaf):
+                histories.append(getHistoryObject(3, {"channel_id": after.channel.id}))
 
-                histories.append(history)
+            # Member undeafened themselves or was undeafened
+            if not (after.self_deaf or after.deaf) and (before.self_deaf or before.deaf):
+                histories.append(getHistoryObject(4, {"channel_id": after.channel.id}))
 
-            # stop streaming upon leaving
-            if before.self_video or before.self_stream:
-                history = History(
-                    discord_id=member.id,
-                    guild_id=member.guild.id,
-                    event_id=6,
-                    additional_info={
-                        "channel_id": before.channel.id,
-                    },
-                )
+            # Member started streaming
+            if (after.self_video or after.self_stream) and not (before.self_stream or before.self_video):
+                histories.append(getHistoryObject(5, {"channel_id": after.channel.id}))
 
-                histories.append(history)
+            # Member stopped streaming
+            if not (after.self_video or after.self_stream) and (before.self_stream or before.self_video):
+                histories.append(getHistoryObject(6, {"channel_id": after.channel.id}))
 
-            # insert last to complete the join-leave-circle
-            histories.append(historyLeave)
+            # last history to save for the join-leave-circle
+            # Member left a voice channel
+            if before.channel and not after.channel:
+                historyLeave = getHistoryObject(8, {"channel_id": before.channel.id, })
 
-        # Member muted themselves or was muted
-        if (after.self_mute or after.mute) and not (before.self_mute or before.mute):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=1,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
+                # unmute member upon leaving
+                if before.self_mute or before.mute:
+                    histories.append(getHistoryObject(2, {"channel_id": before.channel.id}))
 
-            histories.append(history)
+                # undeafen member upon leaving
+                if before.self_deaf or before.deaf:
+                    histories.append(getHistoryObject(4, {"channel_id": before.channel.id}))
 
-        # Member joined a voice channel muted
-        if not before.channel and after.channel and (after.self_mute or after.mute):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=1,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
+                # stop streaming upon leaving
+                if before.self_video or before.self_stream:
+                    histories.append(getHistoryObject(6, {"channel_id": before.channel.id}))
 
-            histories.append(history)
+                # insert last to complete the join-leave-circle
+                histories.append(historyLeave)
 
-        # Member unmuted themselves or was unmuted
-        if not (after.self_mute or after.mute) and (before.self_mute or before.mute):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=2,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
+            if len(histories) == 0:
+                logger.debug(f"No history to save for {member.display_name, member.id}")
 
-            histories.append(history)
+                return
 
-        # Member deafened themselves or was deafened
-        if (after.self_deaf or after.deaf) and not (before.self_deaf or before.deaf):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=3,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
+            with self.session:
+                for history in histories:
+                    try:
+                        self.session.add(history)
+                    except Exception as error:
+                        logger.error(f"Failed to save history for {member.display_name, member.id}", exc_info=error)
 
-            histories.append(history)
+                        continue
 
-        # Member joined a voice channel deafened
-        if not before.channel and after.channel and (after.self_deaf or after.deaf):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=3,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
-
-            histories.append(history)
-
-        # Member undeafened themselves or was undeafened
-        if not (after.self_deaf or after.deaf) and (before.self_deaf or before.deaf):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=4,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
-
-            histories.append(history)
-
-        # Member started streaming
-        if (after.self_video or after.self_stream) and not (before.self_stream or before.self_video):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=5,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
-
-            histories.append(history)
-
-        # Member stopped streaming
-        if not (after.self_video or after.self_stream) and (before.self_stream or before.self_video):
-            history = History(
-                discord_id=member.id,
-                guild_id=member.guild.id,
-                event_id=6,
-                additional_info={
-                    "channel_id": after.channel.id,
-                },
-            )
-
-            histories.append(history)
-
-        if len(histories) == 0:
-            logger.debug(f"No history to save for {member.display_name, member.id}")
-
-            return
-
-        with self.session:
-            for history in histories:
                 try:
-                    self.session.add(history)
+                    self.session.commit()
                 except Exception as error:
-                    logger.error(f"Failed to save history for {member.display_name, member.id}", exc_info=error)
+                    logger.error(f"Failed to save histories for {member.display_name, member.id}", exc_info=error)
 
-                    continue
-
-            try:
-                self.session.commit()
-            except Exception as error:
-                logger.error(f"Failed to save histories for {member.display_name, member.id}", exc_info=error)
-
-                self.session.rollback()
-            else:
-                logger.debug(f"Saved {len(histories)} histories for {member.display_name, member.id}")
+                    self.session.rollback()
+                else:
+                    logger.debug(f"Saved {len(histories)} histories for {member.display_name, member.id}")
