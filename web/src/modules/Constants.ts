a486d4b1c598916923c5c97fec1489cb7418f803
Bjarne <bjarneblu@gmail.com>
2025-03-11 00:02:31 +0100

    docker compose

--- web/src/modules/Constants.ts
+++ web/src/modules/Constants.ts
@@ -1,5 +1,5 @@
 const backendUrl = "";
-const discordOAuthUrl = "https://discord.com/oauth2/authorize?client_id=1076510738576855051&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%2Flogin&scope=identify+email";
+const discordOAuthUrl = "https://discord.com/oauth2/authorize?client_id=1076510738576855051&response_type=code&redirect_uri=https%3A%2F%2Fkvgg.axellotl.de%2Flogin&scope=identify+email";
 const copyrightUrl = "https://github.com/craftundfun";
 
 export {backendUrl, discordOAuthUrl, copyrightUrl};
