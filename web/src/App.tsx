4c7d5f45d28b028ed7bf2342c37a128f97c1b849
axellotl <alex.richter39@gmail.com>
2025-06-10 22:34:20 +0200

    fix build

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR (Hot Modul). Fast as Fuckboiiiii
+          Edit <code>src/App.tsx</code> and save to test HMR (Hot Modul).
         </p>
       </div>
       <p className="read-the-docs">
