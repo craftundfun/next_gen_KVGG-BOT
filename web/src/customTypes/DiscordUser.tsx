f2088c46605178a9eb0f03d7af533bf05c81a4c1
axellotl <alex.richter39@gmail.com>
2025-06-10 20:41:00 +0200

    rm old web.

--- web/src/customTypes/DiscordUser.tsx
+++
@@ -1,8 +0,0 @@
-// import {WebsiteUser} from "./WebsiteUser";
-
-export interface DiscordUser {
-	discord_id: string;
-	global_name: string;
-	created_at: string;
-	//websiteUser?: WebsiteUser;
-}
--- web/src/customTypes/GuildDiscordUserMapping.tsx
+++
@@ -1,8 +0,0 @@
-export interface GuildDiscordUserMapping {
-	guild_id: string,
-	discord_id: string,
-	display_name: string,
-	profile_picture: string | null,
-	joined_at: string,
-	left_at: string | null,
-}
